62c6570f50954a7bdb00e11ef6dd268c42abfbe9
Marshall Slemp <marshall.slemp@gmail.com>
2020-04-13 02:59:06 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -4,6 +4,41 @@ def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  # cart = AoH, coupons = AoH
+  # return new AoH, hash items from carts and new hashes with "ITEM W/COUPON"
+  # loop through cart, each item(hash) needs to be checked in the coupon array for a valid coupon. Check that cart item is in coupon array cart[:item] == coupons[:item], if it doesnt have a coupon push item to new_array 
+  # check cart[:count] >= coupons[:num] than we can apply coupon 
+  # 
+  new_array = []
+  
+  cart.each do |item|
+    
+    item_has_coupon_index = coupons.index do |coupon|
+      coupon[:item] == item[:item]
+    end #check that item has a coupon 
+    
+    if item_has_coupon_index 
+      coupon = coupons[item_has_coupon_index]
+    
+      if item[:count] >= coupon[:num]
+        # apply coupon and update cart 
+        item[:count] -= coupon[:num] 
+        new_array << item 
+        new_item_hash = {
+          :item => "#{coupon[:item]} W/COUPON",
+          :price => coupon[:cost] / coupon[:num],
+          :clearance => item[:clearance],
+          :count => coupon[:num]
+        }
+        new_array << new_item_hash
+      end #end item[count] >= coupon[:num]
+    else
+      # no coupon for item 
+      new_array << item 
+    end #end of item_has_coupon_index
+    
+  end #end of cart.each
+  new_array
 end
 
 def apply_clearance(cart)
